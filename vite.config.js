758880618e59fe11099cf4c29709dcb8ea6db0e0
xiaoliziawa <3682822069@qq.com>
2024-12-02 03:50:42 +0800

    修复错误

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,40 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  base: './',
+  plugins: [
+    vue(),
+    {
+      name: 'html-rewrite',
+      configureServer(server) {
+        return () => {
+          server.middlewares.use((req, _res, next) => {
+            if (req.url?.endsWith('.html')) {
+              req.url = req.url.slice(0, -5)
+            }
+            next()
+          })
+        }
+      }
+    }
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[ext]',
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
+    }
   },
   assetsInclude: ['**/*.md']
 })
